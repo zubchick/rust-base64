f96f0c58a3efedef9744031010aa45093db6e5f1
Nikita Zubkov <zubchick@yandex.ru>
2016-09-28 15:11:19 +0300

    fair decode benchmark

--- benches/benches.rs
+++ benches/benches.rs
@@ -15,15 +15,25 @@ const TEST_PHRASE: &'static str = "Lorem ipsum dolor sit amet, consectetur \
                                    proident, sunt in culpa qui officia \
                                    deserunt mollit anim id est laborum.";
 
+const TEST_ENCODED: &'static str = "TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbn\
+                                    NlY3RldHVyIGFkaXBpc2NpbmcgZWxpdCwgc2VkIGRv\
+                                    IGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbG\
+                                    Fib3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0\
+                                    IGVuaW0gYWQgbWluaW0gdmVuaWFtLCBxdWlzIG5vc3\
+                                    RydWQgZXhlcmNpdGF0aW9uIHVsbGFtY28gbGFib3Jp\
+                                    cyBuaXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kby\
+                                    Bjb25zZXF1YXQuIER1aXMgYXV0ZSBpcnVyZSBkb2xv\
+                                    ciBpbiByZXByZWhlbmRlcml0IGluIHZvbHVwdGF0ZS\
+                                    B2ZWxpdCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVn\
+                                    aWF0IG51bGxhIHBhcmlhdHVy";
+
+
 #[bench]
 fn bench_decoder(b: &mut test::Bencher) {
     let mut out = [0u8; 1024];
-    let count = base64::encode(TEST_PHRASE.as_bytes(), &mut out);
-
-    let mut decode_out = [0u8; 1024];
 
     b.iter(|| {
-        base64::decode(&out[..count], &mut decode_out)
+        base64::decode(TEST_ENCODED.as_bytes(), &mut out)
     })
 }
 
